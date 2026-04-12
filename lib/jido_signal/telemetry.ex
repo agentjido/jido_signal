1f8448be3ff9bd2438a392c83342252ab9e58eda
Mike Hostetler <84222+mikehostetler@users.noreply.github.com>
2026-04-12 13:39:06 -0500

    refactor: remove unused telemetry span helper

--- lib/jido_signal/telemetry.ex
+++ lib/jido_signal/telemetry.ex
@@ -18,19 +18,6 @@ defmodule Jido.Signal.Telemetry do
     :telemetry.execute(event_name, measurements, normalize_metadata(metadata))
   end
 
-  @doc """
-  Emits a telemetry span with package trace metadata merged in.
-
-  The wrapped function should return `{result, stop_metadata}`.
-  """
-  @spec span(event_name(), map(), (-> {term(), map()})) :: term()
-  def span(event_name, metadata, fun) when is_function(fun, 0) do
-    :telemetry.span(event_name, normalize_metadata(metadata), fn ->
-      {result, stop_metadata} = fun.()
-      {result, normalize_metadata(stop_metadata)}
-    end)
-  end
-
   @spec attach(term(), event_name(), function(), map()) :: :ok | {:error, term()}
   def attach(handler_id, event_name, function, config) do
     :telemetry.attach(handler_id, event_name, function, config)
